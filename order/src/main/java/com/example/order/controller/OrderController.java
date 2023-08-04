5e0aa6aa803b96f9b18a16e48ff0aef04bb1f5ab
Park Jae Young <71998296+ye52270@users.noreply.github.com>
2023-08-05 05:22:38 +0900

    1

--- order/src/main/java/com/example/order/controller/OrderController.java
+++ order/src/main/java/com/example/order/controller/OrderController.java
@@ -1,14 +1,17 @@
 package com.example.order.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/order")
 public class OrderController {
     @GetMapping(path = "/test")
     public String orderTest() {
+        log.info("order test success");
         return "order page";
     }
 }
