0e00615323639ac7b851dd8eeb403e6dd1ff0e75
Park Jae Young <71998296+ye52270@users.noreply.github.com>
2023-08-15 15:34:41 +0900

    1
    1

--- member/src/main/java/com/letmego/member/config/CorsConfig.java
+++ member/src/main/java/com/letmego/member/config/CorsConfig.java
@@ -15,7 +15,8 @@ public class CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of("*"));
+//        config.setAllowedOrigins(List.of("http://localhost:5173"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setExposedHeaders(List.of("*"));
